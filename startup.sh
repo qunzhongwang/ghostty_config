64f418bc0c85473796bf509ceca09555004d4f19
qunzhongwang <1155210998@link.cuhk.edu.hk>
2026-03-20 08:41:45 +0800

    macos

--- startup.sh
+++ startup.sh
@@ -1,29 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# shellcheck disable=SC1091
-
-set -euo pipefail
-shopt -s nullglob
-
-# $USER
-if [[ "$(uname)" = 'Darwin' ]]; then
-	logged_in_user=$(echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ {print $3}')
-elif [[ "$(uname)" = 'Linux' ]]; then
-	[[ -n $(logname >/dev/null 2>&1) ]] && logged_in_user=$(logname) || logged_in_user=$(whoami)
-else
-	echo "Unknown OS: $(uname). Exiting..."
-	exit 1
-fi
-
-# $UID
-# logged_in_uid=$(id -u "${logged_in_user}")
-
-# $HOME
-logged_in_home=$(eval echo "~${logged_in_user}")
-
-# Source ~/.bashrc
-. "${logged_in_home}/.bashrc"
-
-if [[ $TERM == "xterm-ghostty" ]] || [[ $TERM = "xterm-256color" ]]; then
-	exec "$SHELL" -li -c "bash" "fastfetch -c ${logged_in_home}/.config/fastfetch.jsonc"
+if [ "$TERM" = "xterm-ghostty" ] || [ "$TERM" = "xterm-256color" ]; then
+	fastfetch -c "$HOME/.config/fastfetch.jsonc"
 fi
